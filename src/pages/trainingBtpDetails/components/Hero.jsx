1ff911293bfe10b5624617dcdda0a2d39504c101
unknown <sirineazzouzi5@gmail.com>
2025-04-16 17:18:30 +0200

    modifier slider home

--- src/pages/trainingBtpDetails/components/Hero.jsx
+++ src/pages/trainingBtpDetails/components/Hero.jsx
@@ -10,10 +10,7 @@ const Hero = ({  formation }) => {
         {formation.description}        </p>
         <div className="flex flex-row items-center gap-4">
         </div>
-        <p className="text-gray-700 text-sm">
-          <span className="text-black/80 font-bold text-base">15</span> sont
-          déjà inscrits
-        </p>
+   
       </div>
       <div className="w-1/2 h-full absolute right-0 bottom-0 top-0 -z-10">
         <img
